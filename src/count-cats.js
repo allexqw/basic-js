c321a8d9b38126524c2c22d3ddc55c5726e7c118
Alexandr Hotyan <alexhotyan@gmail.com>
2020-02-17 22:16:05 +0300

    cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,5 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  //throw 'Not implemented';
+  let str = matrix.join();
+  return str.match(/(\^\^)/g).length;
 };
