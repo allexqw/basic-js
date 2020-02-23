916f0917bf5c2f0f8a7e4008a3db49ff56274dd0
Alexandr Hotyan <alexhotyan@gmail.com>
2020-02-24 02:15:00 +0300

    cats

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,35 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    let array = [];
+    if (Array.isArray(arr)){
+        //console.log(arr);
+        arr.forEach(function(item,index,arr){
+            if (arr[index] == '--discard-next' & index !== arr.length-1){
+                arr.splice(index,2);
+            }
+            else if(arr[index] == '--discard-prev' & index !== 0){
+                arr.splice(index-1,2);
+            }
+            else if(arr[index] == '--double-next' & index !== arr.length-1){
+                arr.splice(index,1,arr[index+1]);
+            }
+            else if (arr[index] == '--double-prev' & index !== 0){
+                arr.splice(index,1,arr[index-1]);
+            }
+            else if(arr[index] == '--double-prev'||arr[index] == '--double-next' || arr[index] == '--discard-next'||arr[index] == '--discard-prev'){
+                
+                arr.splice(index,1);
+                console.log(typeof(arr[index]))
+            } 
+            else{
+                arr[index]=arr[index];
+            }
+        });
+        return arr //= arr.filter(e => e!==null);
+    }
+    else{
+
+        throw (Error);
+    }
+    
+    
 };
